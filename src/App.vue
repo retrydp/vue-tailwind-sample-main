5dd23f71134ee7da3c47eec0ad059475e1b48307
retrydp <thetemon@gmail.com>
2026-01-23 11:31:50 +0200

    test

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex h-screen bg-amber-900 justify-center items-center">
+    <div class="lol ">dsa sample</div>
+  </div>
+</template>
+
+<style scoped></style>
