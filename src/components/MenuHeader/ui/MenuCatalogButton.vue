457df38b61ac498a7868f367c1281c09b991f2c1
retrydp <thetemon@gmail.com>
2026-01-31 15:10:24 +0200

    prettier

--- src/components/MenuHeader/ui/MenuCatalogButton.vue
+++ src/components/MenuHeader/ui/MenuCatalogButton.vue
@@ -1,12 +1,7 @@
 <template>
   <button class="hidden gap-4 p-2 rounded-sm md:flex xl:w-36 bg-def-lime text-white" type="button">
     <div class="flex w-5">
-      <svg
-        width="24px"
-        height="24px"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M4 6H20M4 12H20M4 18H20"
           stroke="currentColor"
@@ -16,6 +11,6 @@
         />
       </svg>
     </div>
-    <p class="hidden text-base  xl:flex">Catalog</p>
+    <p class="hidden text-base xl:flex">Catalog</p>
   </button>
 </template>
--- src/components/MenuHeader/ui/MenuNavbarPersonal.vue
+++ src/components/MenuHeader/ui/MenuNavbarPersonal.vue
@@ -8,21 +8,16 @@ import MenuUserButton from './MenuUserButton.vue';
   >
     <button class="flex flex-col items-center justify-between md:hidden">
       <div class="flex w-5">
-      <svg
-        width="24px"
-        height="24px"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M4 6H20M4 12H20M4 18H20"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </div>
+        <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M4 6H20M4 12H20M4 18H20"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
       <div class="text-[8px] md:text-xs">Catalog</div>
     </button>
     <button class="flex flex-col items-center justify-between hover:text-def-orange">
