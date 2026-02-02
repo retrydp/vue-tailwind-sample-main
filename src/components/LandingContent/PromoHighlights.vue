1aeeed3cbc5083d69f19442646ef2ff2d1975f86
retrydp <thetemon@gmail.com>
2026-02-02 19:04:24 +0200

    fixed center bug

--- src/components/LandingContent/PromoHighlights.vue
+++ src/components/LandingContent/PromoHighlights.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex gap-1 flex-col lg:flex-row justify-center lg:gap-8 xl:gap-10">
-    <div class="flex bg-green-100 rounded-sm basis-1/2">
+    <div class="flex bg-green-100 rounded-sm basis-1/2 items-center">
       <div class="flex flex-col gap-1.5 w-[60%] p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
       <img class="object-scale-down w-1/2 max-h-43" src="@assets/banner_cart.png" alt="" />
     </div>
-    <div class="flex bg-orange-100 rounded-sm basis-1/2">
+    <div class="flex bg-orange-100 rounded-sm basis-1/2 items-center">
       <div class="flex flex-col gap-1.5 w-[60%] p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
