8ca5a32f151ff99ea311c5d6227a74cf12a8343a
retrydp <thetemon@gmail.com>
2026-02-02 19:02:57 +0200

    advantages improved

--- src/components/LandingContent/PromoHighlights.vue
+++ src/components/LandingContent/PromoHighlights.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="flex gap-1 flex-wrap justify-center lg:gap-8 xl:gap-10">
-    <div class="flex h-42 lg:h-45 ls:h-50 bg-orange-200 p-5 rounded-sm grow">
-      <div class="flex flex-col gap-1.5 w-[60%]">
+  <div class="flex gap-1 flex-col lg:flex-row justify-center lg:gap-8 xl:gap-10">
+    <div class="flex bg-green-100 rounded-sm basis-1/2">
+      <div class="flex flex-col gap-1.5 w-[60%] p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
+      <img class="object-scale-down w-1/2 max-h-43" src="@assets/banner_cart.png" alt="" />
     </div>
-    <div class="flex h-42 lg:h-45 ls:h-50 bg-green-100 p-5 rounded-sm grow">
-      <div class="flex flex-col gap-1.5 w-[60%]">
+    <div class="flex bg-orange-100 rounded-sm basis-1/2">
+      <div class="flex flex-col gap-1.5 w-[60%] p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
+      <img class="object-scale-down w-1/2 max-h-43" src="@assets/banner_test.png" alt="" />
     </div>
   </div>
 </template>
-
