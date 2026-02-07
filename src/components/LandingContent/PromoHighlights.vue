b6cec62a86675c9a3062f05730c50a7074303e9a
retrydp <thetemon@gmail.com>
2026-02-07 14:57:18 +0200

    fixed pictures in banners

--- src/components/LandingContent/PromoHighlights.vue
+++ src/components/LandingContent/PromoHighlights.vue
@@ -5,14 +5,22 @@
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
-      <img class="object-scale-down w-1/2 max-h-43" src="@assets/banner_cart.png" alt="" />
+      <img
+        class="object-cover w-full max-h-43 rounded-full overflow-hidden"
+        src="@assets/banner_cart.png"
+        alt=""
+      />
     </div>
     <div class="flex bg-orange-100 rounded-sm basis-1/2 items-center">
       <div class="flex flex-col gap-1.5 w-[60%] p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
-      <img class="object-scale-down w-1/2 max-h-43" src="@assets/banner_test.png" alt="" />
+      <img
+        class="object-cover w-full overflow-hidden max-h-43 rounded-full"
+        src="@assets/banner_test.png"
+        alt=""
+      />
     </div>
   </div>
 </template>
