b95c2f78125af3cc969a724315f8fc37437a4ad3
retrydp <thetemon@gmail.com>
2026-02-10 14:58:25 +0200

    same improvement

--- src/components/LandingContent/PromoHighlights.vue
+++ src/components/LandingContent/PromoHighlights.vue
@@ -1,26 +1,18 @@
 <template>
-  <div class="flex gap-1 flex-col lg:flex-row justify-center lg:gap-8 xl:gap-10">
-    <div class="flex bg-green-100 rounded-sm basis-1/2 items-center">
-      <div class="flex flex-col gap-1.5 w-[60%] p-5">
+  <div class="flex gap-1 flex-col lg:flex-row justify-between lg:gap-8 xl:gap-10">
+    <div class="flex bg-green-100 rounded-sm grow space-between">
+      <div class="flex flex-col gap-1.5 p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
-      <img
-        class="object-cover w-full max-h-43 rounded-full overflow-hidden"
-        src="@assets/banner_cart.png"
-        alt=""
-      />
+      <img class="block object-scale-down w-1/2 max-h-42" src="@assets/banner_cart.png" alt="" />
     </div>
-    <div class="flex bg-orange-100 rounded-sm basis-1/2 items-center">
-      <div class="flex flex-col gap-1.5 w-[60%] p-5">
+    <div class="flex bg-orange-100 rounded-sm grow">
+      <div class="flex flex-col gap-1.5 p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
-      <img
-        class="object-cover w-full overflow-hidden max-h-43 rounded-full"
-        src="@assets/banner_test.png"
-        alt=""
-      />
+      <img class="block object-scale-down w-1/2 max-h-42" src="@assets/banner_test.png" alt="" />
     </div>
   </div>
 </template>
