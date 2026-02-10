3eea5e786d26d3b05498ac441a5dfd34889f1497
retrydp <thetemon@gmail.com>
2026-02-10 15:04:58 +0200

    centered banners

--- src/components/LandingContent/PromoHighlights.vue
+++ src/components/LandingContent/PromoHighlights.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex gap-1 flex-col lg:flex-row justify-between lg:gap-8 xl:gap-10">
-    <div class="flex bg-green-100 rounded-sm grow space-between">
+  <div class="flex gap-1 flex-col md:flex-row justify-between lg:gap-8 xl:gap-10">
+    <div class="flex bg-green-100 rounded-sm grow items-center">
       <div class="flex flex-col gap-1.5 p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
       </div>
       <img class="block object-scale-down w-1/2 max-h-42" src="@assets/banner_cart.png" alt="" />
     </div>
-    <div class="flex bg-orange-100 rounded-sm grow">
+    <div class="flex bg-orange-100 rounded-sm grow items-center">
       <div class="flex flex-col gap-1.5 p-5">
         <p class="font-bold text-xl">Promo Highlights Component</p>
         <p class="">Earn points every time you shop — online or in-store.”</p>
